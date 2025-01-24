027bbf2be0e2e91c4a0e17bf9b74a63e15ef1e8e
Lafuon <abdul.rahman.iv7@gmail.com>
2025-01-24 15:59:38 +0530

    milestone 3 and updated readme

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3001
-
-
-app.listen(port,()=>{
-    console.log("SERVER WORKS!!")
+const express=require('express');
+const mongoose=require('mongoose')
+const app=express()
+const PORT=8084
+let connection= mongoose.connect("mongodb+srv://abdulrahmans66:3Ontsj1rT5n0LsjA@cluster0.vnxb5.mongodb.net/")
+app.get("/ping",(req,res)=>{
+    res.send("pong")
+})
+app.listen(PORT,async()=>{
+try{
+    await connection;
+    console.log("Successfully connected to MongoDB");
+} catch (error){
+    console.log(error);
+}
+    console.log(`Server is running on port ${PORT}`)
 })
